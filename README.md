3d3b7de9c2c28f3d0737a09ff26724ac355c8896
Marco Asbreuk <marco@itsasbreuk.nl>
2015-01-03 01:41:34 +0100

    initial submit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-itags.build
-===========
+For information, api and documentation about this module: visit <b><a href="http://itags.io">itags.io</a></b>
 
-rollup file with all custome elements
+[![Build Status](https://travis-ci.org/itags/itags.build.svg?branch=master)](https://travis-ci.org/itags/itags.build)
+
+[![browser support](https://ci.testling.com/itags/itags.build.png)](https://ci.testling.com/itags/itags.build)
---
+++ itags.build.js
@@ -0,0 +1,23 @@
+/**
+ * The ITSA module is an aggregator for all the individual modules that the library uses.
+ * The developer is free to use it as it is or tailor it to contain whatever modules
+ * he/she might need in the global namespace.
+ *
+ * The modules themselves work quite well independent of this module and can be used
+ * separately without the need of them being integrated under one globa namespace.
+ *
+ *
+ * <i>Copyright (c) 2014 ITSA - https://github.com/itsa</i>
+ * New BSD License - http://choosealicense.com/licenses/bsd-3-clause/
+ *
+ * @module itsa.build
+ *
+*/
+(function (window) {
+
+    "use strict";
+    require('itags.core')(window);
+    require('i-select')(window);
+    require('i-parcel')(window);
+
+})(global.window || require('node-win'));
